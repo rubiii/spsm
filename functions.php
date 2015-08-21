5067d7700847cae80962b647e6497a94c895c156
rubiii <me@rubiii.com>
2015-08-21 17:05:47 +0200

    prevent loading the emoji.min.js file.

--- functions.php
+++ functions.php
@@ -40,3 +40,6 @@ function url_filtered($fields)
   return $fields;
 }
 
+// Prevent loading the emoji JavaScript.
+remove_action("wp_head", "print_emoji_detection_script", 7);
+remove_action("wp_print_styles", "print_emoji_styles");
