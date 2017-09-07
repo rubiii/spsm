588c1b8b7db5528a5ac7c301438fd1e16a3d38bf
rubiii <me@rubiii.com>
2017-09-08 00:10:47 +0200

    Update event list template.

--- shortcode-event-list.php
+++ shortcode-event-list.php
@@ -3,7 +3,7 @@
  * Event List Widget: Standard List
  *
  * The template is used for displaying the [eo_event] shortcode *unless* it is wrapped around a placeholder: e.g. [eo_event] {placeholder} [/eo_event].
- * 
+ *
  * You can use this to edit how the output of the eo_event shortcode. See http://docs.wp-event-organiser.com/shortcodes/events-list
  * For the event list widget see widget-event-list.php
  *
@@ -28,9 +28,7 @@ global $eo_event_loop,$eo_event_loop_args;
 $date_format = "l, j. F Y"; // get_option('date_format');
 $time_format = get_option('time_format');
 
-//The list ID / classes
-$id = ( $eo_event_loop_args['id'] ? 'id="'.$eo_event_loop_args['id'].'"' : '' );
-$classes = $eo_event_loop_args['class'];
+$current_month = "";
 
 ?>
 
@@ -38,15 +36,63 @@ $classes = $eo_event_loop_args['class'];
 
 	<?php while( $eo_event_loop->have_posts() ): $eo_event_loop->the_post(); ?>
 
-		<?php 
-			//Generate HTML classes for this event
-			$eo_event_classes = eo_get_event_classes(); 
-
-			//For non-all-day events, include time format
-			$format = ( eo_is_all_day() ? $date_format : $date_format.' - '.$time_format );
+		<?php
+			if ($current_month != eo_get_the_start("F")) {
+				$current_month = eo_get_the_start("F");
+		?>
+		<h3><?php echo $current_month; ?></h3>
+		<?php
+			}
 		?>
 
-		<strong><?php echo eo_get_the_start($format); ?></strong><br>
+		<?php
+			$start_day = eo_get_the_start("j. F Y");
+			$end_day = eo_get_the_start("j. F Y");
+			$is_multiple_days = $start_day != $end_day;
+
+			if ($is_multiple_days) {
+				if (eo_is_all_day()) {
+					$event_date = eo_get_the_start($date_format) . " bis " . eo_get_the_end($date_format);
+				} else {
+					$event_date = eo_get_the_start($date_format . " - " . $time_format) . " Uhr bis " . eo_get_the_end($date_format . " - " . $time_format) . " Uhr";
+				}
+			} else {
+				if (eo_is_all_day()) {
+					$event_date = eo_get_the_start($date_format);
+				} else {
+					$start_time = eo_get_the_start($time_format);
+					$end_time = eo_get_the_end($time_format);
+
+					if ($start_time == $end_time) {
+						$event_date = eo_get_the_start($date_format . " - " . $time_format) . " Uhr";
+					} else {
+						$event_date = eo_get_the_start($date_format . " - " . $time_format) . "-" . eo_get_the_end($time_format) . " Uhr";
+					}
+				}
+			}
+		?>
+
+		<strong><?php echo get_the_title(); ?></strong><br>
+		<em><?php echo $event_date; ?></em>
+
+    <?php
+      $venueName    = eo_get_venue_name();
+      $venueDetails = eo_get_venue_address();
+      $venueAddress = $venueDetails['address'];
+
+      if ($venueName) {
+        $venue = $venueName;
+
+        if ($venueName != $venueAddress && !empty($venueAddress)) {
+          $venue = $venue . " (" . $venueAddress . ")";
+        }
+    ?>
+       <em>| <?php echo $venue; ?></em>
+    <?php
+      }
+    ?>
+    <br>
+
 		<?php the_content(); ?>
 
 	<?php endwhile; ?>
@@ -56,4 +102,3 @@ $classes = $eo_event_loop_args['class'];
 	<p class="eo-no-events"><?php echo $eo_event_loop_args['no_events']; ?></p>
 
 <?php endif; ?>
-
